449c39e6561184fe9c62666890280b9a6e6f50c0
Mike Morgan <mike.morgan@geckorobotics.com>
2023-04-30 14:14:24 -0400

    made landing page a component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import LandingPage from './components/LandingPage.vue'
 </script>
 
 <template>
-  <div class="grid grid-cols-3 place-items-center h-screen auto-cols-min">
-    <img id="logo-left" class="max-h-60" src="./assets/logos/make_sure_you_have_fun.svg">
-    <div class="verticalLine rounded"></div>
-    <img id="logo-right" class="max-h-56" src="./assets/logos/msyhfm_logo.svg">
-  </div>
-
-
- <RouterView />
+    <LandingPage />
+    <RouterView />
 </template>
 
 <style scoped>
-.verticalLine {
-  border-left: thick solid #FF6600;
-  color: #FF6600;
-  width: 50px;
-  height: 200px;
-}
-#logo-left {
-  margin-right: -600px;
- }
 
-#logo-right {
-  margin-left: -500px;
- }
 </style>
---
+++ frontend/src/components/LandingPage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="grid grid-cols-3 place-items-center h-screen auto-cols-min">
+        <img id="logo-left" class="max-h-60" src="../assets/logos/make_sure_you_have_fun.svg">
+        <div class="verticalLine rounded"></div>
+        <img id="logo-right" class="max-h-56" src="../assets/logos/msyhfm_logo.svg">
+    </div>
+</template>
+
+<style scoped>
+.verticalLine {
+  border-left: thick solid #FF6600;
+  color: #FF6600;
+  width: 50px;
+  height: 200px;
+}
+#logo-left {
+  margin-right: -600px;
+ }
+
+#logo-right {
+  margin-left: -500px;
+ }
+</style>
