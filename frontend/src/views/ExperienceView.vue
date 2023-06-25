a44232daaeb5d93c72b31f13931ae5b479cd4512
Mike Morgan <mike.morgan@geckorobotics.com>
2023-06-24 22:54:32 -0400

    basic home page structure

--- frontend/src/views/ExperienceView.vue
+++ frontend/src/views/ExperienceView.vue
@@ -4,7 +4,110 @@ import NavBar from '../components/NavBar.vue'
 
 <template>
   <main>
-      <h1>Experience Page</h1>
+    <div class="container mx-auto">
+      <div class="flex justify-end px-20 py-5">
+        <div class="font-bold px-10 py-5">MSYH.FM</div>
+        <div class="border-1 col-span-2 border-black bg-orange-500 text-black font-bold py-4 px-20 rounded-full">
+          Player
+        </div>
+      </div>
+
       <NavBar />
+
+      <div class="container mx-auto pt-20 grid grid-cols-2 gap-20">
+
+        <div class="container mx-auto">
+          <div class="container flex space-x-5 mx-auto">
+            <p class="basis-1/2">Photo</p>
+            <div class="flex flex-col">
+              <h1 class="p-5">Title</h1>
+              <h2 class="p-5">Subtitle</h2>
+              <h3 class="p-5">Date</h3>
+            </div>
+          </div>
+
+          <div class="container mx-auto">
+            <ul class="flex space-x-8 place-content-end">
+              <li class="border-1 border-black bg-orange-500 text-black font-bold py-2 px-20 rounded-full">
+                <a>Tickets</a>
+              </li>
+
+              <li class="border-1 border-black bg-orange-500 text-black font-bold py-2 px-20 rounded-full">
+                <a>Facebook</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="container mx-auto">
+          <div class="container flex space-x-5 mx-auto">
+            <p class="basis-1/2">Photo</p>
+            <div class="flex flex-col">
+              <h1 class="p-5">Title</h1>
+              <h2 class="p-5">Subtitle</h2>
+              <h3 class="p-5">Date</h3>
+            </div>
+          </div>
+
+          <div class="container mx-auto">
+            <ul class="flex space-x-8 place-content-end">
+              <li class="border-1 border-black bg-orange-500 text-black font-bold py-2 px-20 rounded-full">
+                <a>Tickets</a>
+              </li>
+
+              <li class="border-1 border-black bg-orange-500 text-black font-bold py-2 px-20 rounded-full">
+                <a>Facebook</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="container mx-auto">
+          <div class="container flex space-x-5 mx-auto">
+            <p class="basis-1/2">Photo</p>
+            <div class="flex flex-col">
+              <h1 class="p-5">Title</h1>
+              <h2 class="p-5">Subtitle</h2>
+              <h3 class="p-5">Date</h3>
+            </div>
+          </div>
+
+          <div class="container mx-auto">
+            <ul class="flex space-x-8 place-content-end">
+              <li class="border-1 border-black bg-orange-500 text-black font-bold py-2 px-20 rounded-full">
+                <a>Tickets</a>
+              </li>
+
+              <li class="border-1 border-black bg-orange-500 text-black font-bold py-2 px-20 rounded-full">
+                <a>Facebook</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="container mx-auto">
+          <div class="container flex space-x-5 mx-auto">
+            <p class="basis-1/2">Photo</p>
+            <div class="flex flex-col">
+              <h1 class="p-5">Title</h1>
+              <h2 class="p-5">Subtitle</h2>
+              <h3 class="p-5">Date</h3>
+            </div>
+          </div>
+
+          <div class="container mx-auto">
+            <ul class="flex space-x-8 place-content-end">
+              <li class="border-1 border-black bg-orange-500 text-black font-bold py-2 px-20 rounded-full">
+                <a>Tickets</a>
+              </li>
+
+              <li class="border-1 border-black bg-orange-500 text-black font-bold py-2 px-20 rounded-full">
+                <a>Facebook</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
