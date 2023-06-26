632c78e3ed2c9f87464a9f12d62d1ac97077b6f4
Mike Morgan <mike.morgan@geckorobotics.com>
2023-06-26 17:42:20 -0400

    added styling to music player

--- frontend/src/views/ExperienceView.vue
+++ frontend/src/views/ExperienceView.vue
@@ -6,8 +6,8 @@ import NavBar from '../components/NavBar.vue'
   <main>
     <div class="container mx-auto">
       <div class="flex justify-end px-20 py-5">
-        <div class="font-bold px-10 py-5">MSYH.FM</div>
-        <div class="border-1 col-span-2 border-black bg-orange-500 text-black font-bold py-4 px-20 rounded-full">
+        <div class="font-bold px-10 py-3">MSYH.FM</div>
+        <div class="border-1 border-black bg-orange-500 px-32 py-3 text-black font-bold w-1/2 h-12 rounded-full">
           Player
         </div>
       </div>
