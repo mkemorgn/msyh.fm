5f88c5f360fd29eba11abc4e5ece010bf6542ad4
Mike Morgan <mike.morgan@geckorobotics.com>
2023-06-26 17:15:22 -0400

    deploy

--- frontend/src/components/LandingPage.vue
+++ frontend/src/components/LandingPage.vue
@@ -2,7 +2,7 @@
   <div class="container mx-auto">
     <div class="flex place-items-center h-screen">
       <div class="flex-auto">
-        <img id="logo-left" class="w-60" src="../assets/logos/make_sure_you_have_fun.svg">
+        <a href="/experience"><img id="logo-left" class="w-60" src="../assets/logos/make_sure_you_have_fun.svg"></a>
       </div>
       <div class="flex-auto">
         <img class="w-60 h-60" src="../assets/logos/center_line.svg">
