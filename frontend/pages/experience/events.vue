08c11de88543a58428bd78beb5aaebe0d49b75e8
Mike Morgan <morgan.mike55@gmail.com>
2023-10-17 06:52:33 -0400

    backing up

--- frontend/pages/experience/events.vue
+++ frontend/pages/experience/events.vue
@@ -1,1 +1,14 @@
-<template>Events</template>
+<template>
+    <div class="grid grid-cols-2 gap-5">
+        <div v-for="e in events">
+            {{ e.event_title }}
+            {{ e.event_details }}
+            {{ e.event_date }}
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+    const { data: events } = await useFetch('http://127.0.0.1:8000/events/')
+</script>
