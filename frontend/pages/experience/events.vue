f2b9003beff0b6b1c378d918371f64fdf356dc5f
Mike Morgan <morgan.mike55@gmail.com>
2023-11-10 15:19:53 -0500

    requirements and events page updated

--- frontend/pages/experience/events.vue
+++ frontend/pages/experience/events.vue
@@ -3,19 +3,17 @@
         <div class="grid grid-rows-3 grid-flow-col container" v-for="e in events">
             <img class="row-span-3 col-span-2" :src="e.event_flyer"/>
 
-            <div class="text-2xl font-bold col-span-2">
+            <div class="text-2xl font-bold col-span-2 -ml-32">
                 {{ e.event_title }}
             </div>
 
-            <div class="text-xl col-span-2">
+            <div class="text-xl col-span-2 -mt-16 -ml-32">
                 {{ e.event_details }}
             </div>
 
-            <div class="text-lg font-bold col-span-2">
+            <div class="text-lg font-bold col-span-2 -mt-32 -ml-32">
                 {{ e.event_date }}
             </div>
-            
-
         </div>
     </div>
 </template>
@@ -23,4 +21,5 @@
 <script setup>
 
     const { data: events } = await useFetch('http://127.0.0.1:8000/events/')
+
 </script>
