f8405ec770078e4ab4493df7b75005f3ce88a078
Mike Morgan <morgan.mike55@gmail.com>
2023-11-11 08:48:48 -0500

    added buttons to events

--- frontend/pages/experience/events.vue
+++ frontend/pages/experience/events.vue
@@ -4,9 +4,11 @@ const { data:events } = await useFetch('http://127.0.0.1:8000/events/')
 
 <template>
     <div class="grid grid-cols-2 gap-5">
-        <div class="grid grid-rows-3 grid-flow-col container" v-for="e in events">
+        <div class="grid grid-rows-4 grid-flow-col gap-10 container" v-for="e in events">
 
-            <img class="row-span-3 col-span-2" :src="e.event_flyer"/>
+            <div class="row-span-4 col-span-2 pr-16">
+                <img :src="e.event_flyer"/>
+            </div>
 
             <div class="text-2xl font-bold col-span-2 -ml-32">
                 {{ e.event_title }}
@@ -18,6 +20,11 @@ const { data:events } = await useFetch('http://127.0.0.1:8000/events/')
 
             <div class="text-lg font-bold col-span-2 -mt-32 -ml-32">
                 {{ e.event_date }} | {{ e.event_time }}
+            </div>
+
+            <div class="text-lg -mt-44 -ml-32">
+                <ButtonButton class="float-left" variant="inverse">Tickets</ButtonButton>
+                <ButtonButton class="float-right" variant="inverse">Facebook</ButtonButton>
             </div>
         </div>
     </div>
