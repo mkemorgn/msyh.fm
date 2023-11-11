a15a2c3c45b504185f92481fc9aa1be52fecc0b9
Mike Morgan <morgan.mike55@gmail.com>
2023-11-11 08:04:13 -0500

    basic formatting for event data

--- backend/msyhfm/events/models.py
+++ backend/msyhfm/events/models.py
@@ -4,8 +4,9 @@ from django.db import models
 class Event(models.Model):
     event_title = models.CharField(max_length=200)
     event_details = models.CharField(max_length=200)
-    event_date = models.DateTimeField("date published")
+    event_date = models.DateField(null=True)
+    event_time = models.TimeField(null=True)
     event_flyer = models.ImageField(null=True, upload_to="images/")
 
     def __str__(self):
-        return f"{self.event_title} {self.event_details} {self.event_date} {self.event_flyer}"
+        return f"{self.event_title} {self.event_details} {self.event_date} {self.event_time} {self.event_flyer}"
--- frontend/pages/experience/events.vue
+++ frontend/pages/experience/events.vue
@@ -1,6 +1,11 @@
+<script setup>
+const { data:events } = await useFetch('http://127.0.0.1:8000/events/')
+</script>
+
 <template>
     <div class="grid grid-cols-2 gap-5">
         <div class="grid grid-rows-3 grid-flow-col container" v-for="e in events">
+
             <img class="row-span-3 col-span-2" :src="e.event_flyer"/>
 
             <div class="text-2xl font-bold col-span-2 -ml-32">
@@ -12,14 +17,8 @@
             </div>
 
             <div class="text-lg font-bold col-span-2 -mt-32 -ml-32">
-                {{ e.event_date }}
+                {{ e.event_date }} | {{ e.event_time }}
             </div>
         </div>
     </div>
 </template>
-
-<script setup>
-
-    const { data: events } = await useFetch('http://127.0.0.1:8000/events/')
-
-</script>
