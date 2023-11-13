6dcee638c3e764f77d7c126f2d57392bb2a1c970
Mike Morgan <morgan.mike55@gmail.com>
2023-11-13 17:00:08 -0500

    more event styling

--- frontend/pages/experience/events.vue
+++ frontend/pages/experience/events.vue
@@ -4,27 +4,28 @@ const { data:events } = await useFetch('http://127.0.0.1:8000/events/')
 
 <template>
     <div class="grid grid-cols-2 gap-5">
-        <div class="grid grid-rows-4 grid-flow-col gap-10 container" v-for="e in events">
-
+        <div class="grid grid-rows-4 grid-flow-col gap-5" v-for="e in events">
             <div class="row-span-4 col-span-2 pr-16">
                 <img :src="e.event_flyer"/>
             </div>
 
-            <div class="text-2xl font-bold col-span-2 -ml-32">
-                {{ e.event_title }}
+            <div class="col-span-2 -ml-28">
+                <div class="text-2xl font-bold">
+                    {{ e.event_title }}
+                </div>
+
+                <div class="text-xl">
+                    {{ e.event_details }}
+                </div>
+
+                <div class="text-lg font-bold">
+                    {{ e.event_date }} | {{ e.event_time }}
+                </div>
             </div>
 
-            <div class="text-xl col-span-2 -mt-16 -ml-32">
-                {{ e.event_details }}
-            </div>
-
-            <div class="text-lg font-bold col-span-2 -mt-32 -ml-32">
-                {{ e.event_date }} | {{ e.event_time }}
-            </div>
-
-            <div class="text-lg -mt-44 -ml-32">
+            <div class="text-lg font-bold -ml-32 ">
                 <ButtonButton class="float-left" variant="inverse">Tickets</ButtonButton>
-                <ButtonButton class="float-right" variant="inverse">Facebook</ButtonButton>
+                <ButtonButton class="float-right -ml-10" variant="inverse">Facebook</ButtonButton>
             </div>
         </div>
     </div>
