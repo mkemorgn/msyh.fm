ab9f29379cbc569cc16b687e34e13391c700878b
Mike Morgan <morgan.mike55@gmail.com>
2024-01-04 11:35:08 -0500

    placeholders for about section

--- frontend/pages/learn/team.vue
+++ frontend/pages/learn/team.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>
-    team
+  <div class="flex justify-around flex-wrap pt-32">
+    <!-- Team Member Component -->
+    <div class="w-64 mb-4">
+      <div class="bg-orange-500 p-1">
+        <div class="bg-gray-300 h-64"></div> <!-- Placeholder for the image -->
+      </div>
+      <div class="text-center mt-2">
+        <div class="font-bold">Ryan "RB" Brown</div>
+        <div class="text-gray-700">Founder/Talent Buyer</div>
+      </div>
+    </div>
+    <div class="w-64 mb-4">
+      <div class="bg-orange-500 p-1">
+        <div class="bg-gray-300 h-64"></div> <!-- Placeholder for the image -->
+      </div>
+      <div class="text-center mt-2">
+        <div class="font-bold">Mike Morgan</div>
+        <div class="text-gray-700">Web Developer</div>
+      </div>
+    </div>
+    <div class="w-64 mb-4">
+      <div class="bg-orange-500 p-1">
+        <div class="bg-gray-300 h-64"></div> <!-- Placeholder for the image -->
+      </div>
+      <div class="text-center mt-2">
+        <div class="font-bold">Sara Coffey</div>
+        <div class="text-gray-700">UX & Usability Analyst</div>
+      </div>
+    </div>
   </div>
 </template>
