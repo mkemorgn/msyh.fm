3c5368a938ac7dec02ffb1f24f9c4e12771fd2a9
Mike Morgan <morgan.mike55@gmail.com>
2024-01-09 17:38:33 -0500

    added another sleep so app has time to start

--- setup.sh
+++ setup.sh
@@ -3,15 +3,12 @@
 # set up dev workspace opens two alacritty windows and starts projects.
 
 alacritty --title "backend" --command zsh -c 'cd backend && source venv/bin/activate && cd msyhfm && python manage.py runserver' &
-
 sleep 1
-
 i3-msg '[title="backend"]' move container to workspace 9
 
 alacritty --title "frontend" --command zsh -c 'cd frontend && npm run dev' &
-
 sleep 1
-
 i3-msg '[title="frontend"]' move container to workspace 9
 
+sleep 2
 firefox http://localhost:3000 &
